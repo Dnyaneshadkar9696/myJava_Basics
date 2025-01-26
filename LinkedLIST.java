0cd4366916b458eaa82742bedd1230642444d19a
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-01-26 11:47:32 +0530

    Add files via upload
    The linkedList declaration.

---
+++ LinkedLIST.java
@@ -0,0 +1,20 @@
+package LinkedList01;
+
+import java.util.LinkedList;
+
+public class LinkedLIST {
+
+    public static void main(String[] args) {
+//     creating the object of the linked List
+        LinkedList<Integer> ll  = new LinkedList<Integer>();
+//        Creation of LinkedList is succesfull
+
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        System.out.println("Integers in the linkedlist : "+ ll);
+
+    }
+}
