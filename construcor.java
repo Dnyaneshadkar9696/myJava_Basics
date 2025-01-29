6e9e2d118545a8a6ef88e3370b8f5ca53811e816
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-01-29 22:56:28 +0530

    Add files via upload
    The Basic Constructor.

---
+++ construcor.java
@@ -0,0 +1,23 @@
+
+
+class hau{
+
+    hau() {
+
+
+        int a = 4;
+        int b = 9;
+        int c = 0;
+        c = a + b;
+        System.out.println(c);
+
+
+    }
+}
+
+public class construcor {
+    public static void main(String[] args){
+//        the constructor is called when the object is not stored in the variable
+        new hau();
+    }
+}
